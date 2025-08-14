4fadef9078ffd81469ab81fbd81e1b15752f6f2a
Documenter.jl <documenter@juliadocs.github.io>
2025-08-14 08:29:09 +0000

    build based on fc0d11c

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LearningSchedules","category":"page"},{"location":"#LearningSchedules","page":"Home","title":"LearningSchedules","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LearningSchedules.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LearningSchedules]","category":"page"}]
+[{"location":"#LearningSchedules","page":"Home","title":"LearningSchedules","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LearningSchedules.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
